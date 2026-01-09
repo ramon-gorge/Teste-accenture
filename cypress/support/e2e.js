ad0ad2a4106a825d50e6ac6618042d6898cbb035
ramon-gorge <44533001+ramon-gorge@users.noreply.github.com>
2026-01-09 11:57:28 -0300

    Clean up duplicate imports in e2e.js
    Removed duplicate imports in e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,9 +2,3 @@ import './api_commands'
 import './gui_commands'
 import '@faker-js/faker'
 import 'cypress-plugin-api'
-
-// Import commands.js using ES2015 syntax:
-import './api_commands'
-import './gui_commands'
-import '@faker-js/faker'
-import 'cypress-plugin-api'
